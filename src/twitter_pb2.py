5ea421d0a5f7138669eac0cd34be9ce24cd8f361
hinha <martinuz.dawan9@gmail.com>
2020-11-01 13:21:23 +0700

    update proto

--- src/twitter_pb2.py
+++ src/twitter_pb2.py
@@ -6,6 +6,7 @@ from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
+
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -18,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='twitter',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=b'\n\x13proto/twitter.proto\x12\x07twitter\x1a\x0fvalidator.proto\"\x92\x07\n\x04Item\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x11\n\tpermalink\x18\x02 \x01(\t\x12\x11\n\ttimestamp\x18\x03 \x01(\x03\x12\x12\n\ncreated_at\x18\x04 \x01(\t\x12\x17\n\x0f\x63reated_at_time\x18\x05 \x01(\t\x12\x16\n\x0e\x66ull_text_norm\x18\x06 \x01(\t\x12\x17\n\x0f\x66ull_text_clean\x18\x07 \x01(\t\x12\x11\n\tfull_text\x18\x08 \x01(\t\x12\x16\n\x0etext_sentiment\x18\t \x01(\t\x12\x0f\n\x07user_id\x18\n \x01(\t\x12\x11\n\tuser_name\x18\x0b \x01(\t\x12\x13\n\x0bscreen_name\x18\x0c \x01(\t\x12\x18\n\x10user_description\x18\r \x01(\t\x12\x15\n\ruser_verified\x18\x0e \x01(\x08\x12\x17\n\x0fuser_created_at\x18\x0f \x01(\t\x12\x1c\n\x14user_followers_count\x18\x10 \x01(\x05\x12\x1a\n\x12user_friends_count\x18\x11 \x01(\x05\x12\x1b\n\x13user_statuses_count\x18\x12 \x01(\x05\x12\x1d\n\x15user_favourites_count\x18\x13 \x01(\x05\x12\x19\n\x11user_listed_count\x18\x14 \x01(\x05\x12\x18\n\x10user_media_count\x18\x15 \x01(\x05\x12\x15\n\ruser_location\x18\x16 \x01(\x05\x12\x16\n\x0euser_protected\x18\x17 \x01(\x08\x12\x15\n\ruser_link_url\x18\x18 \x01(\t\x12\x1e\n\x16user_profile_image_url\x18\x19 \x01(\t\x12\x13\n\x0breply_count\x18\x1a \x01(\x05\x12\x15\n\rretweet_count\x18\x1b \x01(\x05\x12\x12\n\nlike_count\x18\x1c \x01(\x05\x12\x13\n\x0bquote_count\x18\x1d \x01(\x05\x12\x17\n\x0f\x63onversation_id\x18\x1e \x01(\x05\x12\x0e\n\x06\x64\x65vice\x18\x1f \x01(\t\x12\x0c\n\x04lang\x18  \x01(\t\x12!\n\x05media\x18! \x03(\x0b\x32\x12.twitter.mediaItem\x12\"\n\x08mentions\x18\" \x03(\x0b\x32\x10.twitter.Mention\x12\"\n\x08hashtags\x18# \x03(\x0b\x32\x10.twitter.Hashtag\x12\x12\n\nengagement\x18$ \x01(\x03\x12\x17\n\x0fpotential_reach\x18% \x01(\x03\x12\x17\n\x0f\x65ngagement_rate\x18& \x01(\x02\":\n\tmediaItem\x12\x0c\n\x04type\x18\x01 \x01(\t\x12\x1f\n\x06source\x18\x02 \x03(\x0b\x32\x0f.twitter.Source\"/\n\x06Source\x12\x13\n\x0bpreview_url\x18\x01 \x01(\t\x12\x10\n\x08\x66ull_url\x18\x02 \x01(\t\"-\n\x07Mention\x12\x0c\n\x04text\x18\x01 \x01(\t\x12\x14\n\x0c\x64isplay_name\x18\x02 \x01(\t\"\x17\n\x07Hashtag\x12\x0c\n\x04text\x18\x01 \x01(\t\"p\n\x0etwitterRequest\x12\x16\n\x07keyword\x18\x01 \x01(\tB\x05\x9a\x42\x02`\x01\x12\x1a\n\x0bsearch_type\x18\x02 \x01(\tB\x05\x9a\x42\x02`\x01\x12\x14\n\x05since\x18\x03 \x01(\tB\x05\x9a\x42\x02`\x01\x12\x14\n\x05until\x18\x04 \x01(\tB\x05\x9a\x42\x02`\x01\"R\n\x0ftwitterResponse\x12\x0f\n\x07message\x18\x01 \x01(\t\x12\x10\n\x08updateAt\x18\x02 \x01(\t\x12\x1c\n\x05items\x18\x03 \x03(\x0b\x32\r.twitter.Item2O\n\x07twitter\x12\x44\n\rStreamRequest\x12\x17.twitter.twitterRequest\x1a\x18.twitter.twitterResponse\"\x00\x62\x06proto3'
+  serialized_pb=b'\n\x13proto/twitter.proto\x12\x07twitter\x1a\x0fvalidator.proto\"\xae\x08\n\x04Item\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x11\n\tpermalink\x18\x02 \x01(\t\x12\x11\n\ttimestamp\x18\x03 \x01(\x03\x12\x12\n\ncreated_at\x18\x04 \x01(\t\x12\x17\n\x0f\x63reated_at_time\x18\x05 \x01(\t\x12\x16\n\x0e\x66ull_text_norm\x18\x06 \x01(\t\x12\x17\n\x0f\x66ull_text_clean\x18\x07 \x01(\t\x12\x11\n\tfull_text\x18\x08 \x01(\t\x12\x16\n\x0etext_sentiment\x18\t \x01(\t\x12\x0f\n\x07user_id\x18\n \x01(\t\x12\x11\n\tuser_name\x18\x0b \x01(\t\x12\x13\n\x0bscreen_name\x18\x0c \x01(\t\x12\x18\n\x10user_description\x18\r \x01(\t\x12\x15\n\ruser_verified\x18\x0e \x01(\x08\x12\x17\n\x0fuser_created_at\x18\x0f \x01(\t\x12\x1c\n\x14user_followers_count\x18\x10 \x01(\x05\x12\x1a\n\x12user_friends_count\x18\x11 \x01(\x05\x12\x1b\n\x13user_statuses_count\x18\x12 \x01(\x05\x12\x1d\n\x15user_favourites_count\x18\x13 \x01(\x05\x12\x19\n\x11user_listed_count\x18\x14 \x01(\x05\x12\x18\n\x10user_media_count\x18\x15 \x01(\x05\x12\x15\n\ruser_location\x18\x16 \x01(\t\x12\x16\n\x0euser_protected\x18\x17 \x01(\x08\x12\x15\n\ruser_link_url\x18\x18 \x01(\t\x12\x1e\n\x16user_profile_image_url\x18\x19 \x01(\t\x12\x13\n\x0breply_count\x18\x1a \x01(\x05\x12\x15\n\rretweet_count\x18\x1b \x01(\x05\x12\x12\n\nlike_count\x18\x1c \x01(\x05\x12\x13\n\x0bquote_count\x18\x1d \x01(\x05\x12\x17\n\x0f\x63onversation_id\x18\x1e \x01(\x03\x12\x0e\n\x06\x64\x65vice\x18\x1f \x01(\t\x12\x0c\n\x04lang\x18  \x01(\t\x12!\n\x05media\x18! \x03(\x0b\x32\x12.twitter.mediaItem\x12\"\n\x08mentions\x18\" \x03(\x0b\x32\x10.twitter.Mention\x12\"\n\x08hashtags\x18# \x03(\x0b\x32\x10.twitter.Hashtag\x12\x12\n\nengagement\x18$ \x01(\x03\x12\x17\n\x0fpotential_reach\x18% \x01(\x03\x12\x17\n\x0f\x65ngagement_rate\x18& \x01(\x02\x12\x12\n\nplace_type\x18\' \x01(\t\x12\x12\n\nplace_name\x18( \x01(\t\x12\x14\n\x0c\x63ountry_code\x18) \x01(\t\x12\x19\n\x11\x62ounding_box_type\x18* \x01(\t\x12\x41\n\x18\x62ounding_box_coordinates\x18+ \x03(\x0b\x32\x1f.twitter.BoundingBoxCoordinates\"=\n\x16\x42oundingBoxCoordinates\x12\x11\n\tlongitude\x18\x01 \x01(\x02\x12\x10\n\x08latitude\x18\x02 \x01(\x02\":\n\tmediaItem\x12\x0c\n\x04type\x18\x01 \x01(\t\x12\x1f\n\x06source\x18\x02 \x03(\x0b\x32\x0f.twitter.Source\"X\n\x06Source\x12\x13\n\x0bpreview_url\x18\x01 \x01(\t\x12\x10\n\x08\x66ull_url\x18\x02 \x01(\t\x12\x15\n\rthumbnail_url\x18\x03 \x01(\t\x12\x10\n\x08\x64uration\x18\x04 \x01(\t\"9\n\x07Mention\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0c\n\x04text\x18\x02 \x01(\t\x12\x14\n\x0c\x64isplay_name\x18\x03 \x01(\t\"\x17\n\x07Hashtag\x12\x0c\n\x04text\x18\x01 \x01(\t\"p\n\x0etwitterRequest\x12\x16\n\x07keyword\x18\x01 \x01(\tB\x05\x9a\x42\x02`\x01\x12\x1a\n\x0bsearch_type\x18\x02 \x01(\tB\x05\x9a\x42\x02`\x01\x12\x14\n\x05since\x18\x03 \x01(\tB\x05\x9a\x42\x02`\x01\x12\x14\n\x05until\x18\x04 \x01(\tB\x05\x9a\x42\x02`\x01\"R\n\x0ftwitterResponse\x12\x0f\n\x07message\x18\x01 \x01(\t\x12\x10\n\x08updateAt\x18\x02 \x01(\t\x12\x1c\n\x05items\x18\x03 \x03(\x0b\x32\r.twitter.Item2O\n\x07twitter\x12\x44\n\rStreamRequest\x12\x17.twitter.twitterRequest\x1a\x18.twitter.twitterResponse\"\x00\x62\x06proto3'
   ,
   dependencies=[validator__pb2.DESCRIPTOR,])
 
@@ -181,8 +182,8 @@ _ITEM = _descriptor.Descriptor(
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='user_location', full_name='twitter.Item.user_location', index=21,
-      number=22, type=5, cpp_type=1, label=1,
-      has_default_value=False, default_value=0,
+      number=22, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
@@ -237,7 +238,7 @@ _ITEM = _descriptor.Descriptor(
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='conversation_id', full_name='twitter.Item.conversation_id', index=29,
-      number=30, type=5, cpp_type=1, label=1,
+      number=30, type=3, cpp_type=2, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -298,6 +299,41 @@ _ITEM = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='place_type', full_name='twitter.Item.place_type', index=38,
+      number=39, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='place_name', full_name='twitter.Item.place_name', index=39,
+      number=40, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='country_code', full_name='twitter.Item.country_code', index=40,
+      number=41, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='bounding_box_type', full_name='twitter.Item.bounding_box_type', index=41,
+      number=42, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='bounding_box_coordinates', full_name='twitter.Item.bounding_box_coordinates', index=42,
+      number=43, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -311,7 +347,45 @@ _ITEM = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=50,
-  serialized_end=964,
+  serialized_end=1120,
+)
+
+
+_BOUNDINGBOXCOORDINATES = _descriptor.Descriptor(
+  name='BoundingBoxCoordinates',
+  full_name='twitter.BoundingBoxCoordinates',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='longitude', full_name='twitter.BoundingBoxCoordinates.longitude', index=0,
+      number=1, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='latitude', full_name='twitter.BoundingBoxCoordinates.latitude', index=1,
+      number=2, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1122,
+  serialized_end=1183,
 )
 
 
@@ -348,8 +422,8 @@ _MEDIAITEM = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=966,
-  serialized_end=1024,
+  serialized_start=1185,
+  serialized_end=1243,
 )
 
 
@@ -374,6 +448,20 @@ _SOURCE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='thumbnail_url', full_name='twitter.Source.thumbnail_url', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='duration', full_name='twitter.Source.duration', index=3,
+      number=4, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -386,8 +474,8 @@ _SOURCE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1026,
-  serialized_end=1073,
+  serialized_start=1245,
+  serialized_end=1333,
 )
 
 
@@ -399,15 +487,22 @@ _MENTION = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='text', full_name='twitter.Mention.text', index=0,
-      number=1, type=9, cpp_type=9, label=1,
+      name='id', full_name='twitter.Mention.id', index=0,
+      number=1, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='text', full_name='twitter.Mention.text', index=1,
+      number=2, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='display_name', full_name='twitter.Mention.display_name', index=1,
-      number=2, type=9, cpp_type=9, label=1,
+      name='display_name', full_name='twitter.Mention.display_name', index=2,
+      number=3, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -424,8 +519,8 @@ _MENTION = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1075,
-  serialized_end=1120,
+  serialized_start=1335,
+  serialized_end=1392,
 )
 
 
@@ -455,8 +550,8 @@ _HASHTAG = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1122,
-  serialized_end=1145,
+  serialized_start=1394,
+  serialized_end=1417,
 )
 
 
@@ -507,8 +602,8 @@ _TWITTERREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1147,
-  serialized_end=1259,
+  serialized_start=1419,
+  serialized_end=1531,
 )
 
 
@@ -552,16 +647,18 @@ _TWITTERRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1261,
-  serialized_end=1343,
+  serialized_start=1533,
+  serialized_end=1615,
 )
 
 _ITEM.fields_by_name['media'].message_type = _MEDIAITEM
 _ITEM.fields_by_name['mentions'].message_type = _MENTION
 _ITEM.fields_by_name['hashtags'].message_type = _HASHTAG
+_ITEM.fields_by_name['bounding_box_coordinates'].message_type = _BOUNDINGBOXCOORDINATES
 _MEDIAITEM.fields_by_name['source'].message_type = _SOURCE
 _TWITTERRESPONSE.fields_by_name['items'].message_type = _ITEM
 DESCRIPTOR.message_types_by_name['Item'] = _ITEM
+DESCRIPTOR.message_types_by_name['BoundingBoxCoordinates'] = _BOUNDINGBOXCOORDINATES
 DESCRIPTOR.message_types_by_name['mediaItem'] = _MEDIAITEM
 DESCRIPTOR.message_types_by_name['Source'] = _SOURCE
 DESCRIPTOR.message_types_by_name['Mention'] = _MENTION
@@ -576,6 +673,13 @@ Item = _reflection.GeneratedProtocolMessageType('Item', (_message.Message,), {
   # @@protoc_insertion_point(class_scope:twitter.Item)
   })
 _sym_db.RegisterMessage(Item)
+
+BoundingBoxCoordinates = _reflection.GeneratedProtocolMessageType('BoundingBoxCoordinates', (_message.Message,), {
+  'DESCRIPTOR' : _BOUNDINGBOXCOORDINATES,
+  '__module__' : 'proto.twitter_pb2'
+  # @@protoc_insertion_point(class_scope:twitter.BoundingBoxCoordinates)
+  })
+_sym_db.RegisterMessage(BoundingBoxCoordinates)
 
 mediaItem = _reflection.GeneratedProtocolMessageType('mediaItem', (_message.Message,), {
   'DESCRIPTOR' : _MEDIAITEM,
@@ -631,8 +735,8 @@ _TWITTER = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=None,
-  serialized_start=1345,
-  serialized_end=1424,
+  serialized_start=1617,
+  serialized_end=1696,
   methods=[
   _descriptor.MethodDescriptor(
     name='StreamRequest',
